570f40ed9293a524989660c5ff1ce30d08287e2c
Nikita Mikhailovskiy <nmihaylovskiy@gmail.com>
2024-01-12 21:01:39 +0600

    Update task description and cmake

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,7 @@
 
 int main(int argc, char const *argv[]) {
   struct ProjectInfo info = {};
-
   std::cout << info.nameString << "\t" << info.versionString << '\n';
-  std::cout << "Welcome from CMake template!\n" << std::endl;
 
   (void)argc;
   (void)argv;
